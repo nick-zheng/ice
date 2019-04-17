3130cfd9010caa07e53520b46104ab6b597d542a
alvinhui <alvin.hui@qq.com>
2019-04-17 19:34:09 +0800

    feat: init store

--- packages/iceworks-client/src/index.js
+++ packages/iceworks-client/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { StoreContext } from 'redux-react-hook';
 import '@alifd/next/reset.scss';
 
 import MainLayout from './layouts/MainLayout';
 import LocaleProvider from './components/LocaleProvider';
 import { getLocale } from './utils/locale';
+import makeStore from './store';
 
+const store = makeStore();
 const locale = getLocale();
 const ICE_CONTAINER = document.getElementById('ice-container');
 
 ReactDOM.render(
-  <LocaleProvider locale={locale}>
-    <Router>
-      <Route path="/" component={MainLayout} />
-    </Router>
-  </LocaleProvider>,
+  <StoreContext.Provider value={store}>
+    <LocaleProvider locale={locale}>
+      <Router>
+        <Route path="/" component={MainLayout} />
+      </Router>
+    </LocaleProvider>
+  </StoreContext.Provider>,
   ICE_CONTAINER,
 );
--- packages/iceworks-client/src/pages/Project/index.js
+++ packages/iceworks-client/src/pages/Project/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { Button, Input } from '@alifd/next';
 
 export default class Project extends Component {
   state = {};
 
+  onOpenProject = () => {
+  }
+
   render() {
-    return <div>Project</div>;
+    return (
+      <div>
+        <div>
+          Project
+        </div>
+        <div>
+          <div>
+            <Input onChange={(value) => {
+                this.projectPath = value;
+              }}
+            />
+            <Button onClick={this.onOpenProject}>打开项目</Button>
+          </div>
+          <div>
+            <Button>创建项目</Button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
